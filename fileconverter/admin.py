60338d5ca9439a3dd1aeaa42a8df5c6a89162744
alwaysnithish <inithish21k@gmail.com>
2025-10-05 14:27:39 +0530

    Delete fileconverter directory

--- fileconverter/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Conversion
-
-@admin.register(Conversion)
-class ConversionAdmin(admin.ModelAdmin):
-    list_display = ['original_filename', 'original_format', 'target_format', 'success', 'created_at']
-    list_filter = ['success', 'original_format', 'target_format', 'created_at']
-    search_fields = ['original_filename', 'converted_filename']
